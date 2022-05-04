06bafe7b2748afbc713a3b7b59a0b7af837418eb
Dashui.Fu <13777895428@163.com>
2022-05-05 00:12:46 +0800

    [A]扩展原理

--- src/main/java/com/match/bean/Blue.java
+++ src/main/java/com/match/bean/Blue.java
@@ -4,4 +4,17 @@ package com.match.bean;
  * @author matchfu
  */
 public class Blue {
+
+    public Blue() {
+        System.out.println("Blue constructor...");
+    }
+
+    public void init() {
+        System.out.println("Blue ... init...");
+    }
+
+    public void destroy() {
+        System.out.println("Blue ... destroy");
+    }
+
 }
